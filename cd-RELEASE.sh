b1cb7af7409231f8a8f8ba96950145891d3995af
ElvisFabian <elvis.fabian@sigma.com.br>
2019-09-03 16:35:22 -0400

    OK

--- cd-RELEASE.sh
+++ cd-RELEASE.sh
@@ -13,26 +13,22 @@ export NPM_LIFE_CICLE_VERSION=dev
 
 echo "---------Publico a imagem final em dev"
 docker-compose -f "docker-compose.cd-release.yml" up --build --abort-on-container-exit
-docker-compose -f "docker-compose.cd-release.yml" down -v
-rm -rf *.yaml
+docker-compose -f "docker-compose.cd-release.yml" down
 
 echo "---------Publico a imagem final em qa"
 export ENVIRONMENT=qa
 export NPM_LIFE_CICLE_VERSION=qa
 docker-compose -f "docker-compose.cd-release.yml" up --build --abort-on-container-exit
-docker-compose -f "docker-compose.cd-release.yml" down -v
-rm -rf *.yaml
+docker-compose -f "docker-compose.cd-release.yml" down
 
 echo "---------Publico a imagem final em stage"
 export ENVIRONMENT=stage
 export NPM_LIFE_CICLE_VERSION=stage
 docker-compose -f "docker-compose.cd-release.yml" up --build --abort-on-container-exit
-docker-compose -f "docker-compose.cd-release.yml" down -v
-rm -rf *.yaml
+docker-compose -f "docker-compose.cd-release.yml" down
 
 echo "---------Publico a imagem final em prod"
 export ENVIRONMENT=prod
 export NPM_LIFE_CICLE_VERSION=""
 docker-compose -f "docker-compose.cd-release.yml" up --build --abort-on-container-exit
-docker-compose -f "docker-compose.cd-release.yml" down -v
-rm -rf *.yaml
+docker-compose -f "docker-compose.cd-release.yml" down
