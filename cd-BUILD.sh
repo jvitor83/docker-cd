d1b99c2b2aecf4192027a592b6e1f1af08a3c179
ElvisFabian <elvis.fabian@sigma.com.br>
2019-09-04 16:55:50 -0400

    .

--- cd-BUILD.sh
+++ cd-BUILD.sh
@@ -1,5 +1,6 @@
 clear
 rm -rf ./docker-extract/
+mkdir ./docker-extract/
 
 #Essas variaveis precisam estar na release também
 export DOCKER_REGISTRY=nexusdocker.tjmt.jus.br/dsa/teste/
@@ -20,11 +21,11 @@ export DOCKERCOMPOSE_BUILD_CONTAINER_NAME="container-testResults"
 export DOCKERCOMPOSE_BUILD_TEST_RESULT_PATH="/TestResults"
 
 docker-compose -f "docker-compose.yml" -f "docker-compose.cd-build.yml" up --build --abort-on-container-exit
-docker-compose -f "docker-compose.yml" -f "docker-compose.cd-build.yml" push
+#docker-compose -f "docker-compose.yml" -f "docker-compose.cd-build.yml" push
 
 echo "Extraindo os resultados dos testes"
 docker create --name $DOCKERCOMPOSE_BUILD_CONTAINER_NAME -v $DOCKERCOMPOSE_BUILD_VOLUME_NAME:$DOCKERCOMPOSE_BUILD_TEST_RESULT_PATH busybox
-docker cp $DOCKERCOMPOSE_BUILD_CONTAINER_NAME:$DOCKERCOMPOSE_BUILD_TEST_RESULT_PATH $ARTIFACT_STAGING_DIRECTORY
+docker cp $DOCKERCOMPOSE_BUILD_CONTAINER_NAME:$DOCKERCOMPOSE_BUILD_TEST_RESULT_PATH $ARTIFACT_STAGING_DIRECTORY/TestResults
 docker rm $DOCKERCOMPOSE_BUILD_CONTAINER_NAME
 echo "-------------------------------------------"
 
@@ -37,11 +38,11 @@ export DOCKERCOMPOSE_PUBLISH_CONTAINER_NAME="container-publish"
 export DOCKERCOMPOSE_PUBLISH_APP_PATH="/var/release/"
 
 docker-compose -f "docker-compose.yml" -f "docker-compose.cd-publish.yml" up --build --abort-on-container-exit
-docker-compose -f "docker-compose.yml" -f "docker-compose.cd-publish.yml" push
+#docker-compose -f "docker-compose.yml" -f "docker-compose.cd-publish.yml" push
 
 echo "Extraindo o artefatos"
 docker create --name $DOCKERCOMPOSE_PUBLISH_CONTAINER_NAME -v $DOCKERCOMPOSE_PUBLISH_VOLUME_NAME:$DOCKERCOMPOSE_PUBLISH_APP_PATH busybox
-docker cp $DOCKERCOMPOSE_PUBLISH_CONTAINER_NAME:$DOCKERCOMPOSE_PUBLISH_APP_PATH $ARTIFACT_STAGING_DIRECTORY
+docker cp $DOCKERCOMPOSE_PUBLISH_CONTAINER_NAME:$DOCKERCOMPOSE_PUBLISH_APP_PATH $ARTIFACT_STAGING_DIRECTORY/artefatos
 docker rm $DOCKERCOMPOSE_PUBLISH_CONTAINER_NAME
 
 
@@ -49,7 +50,7 @@ echo ""
 echo "-------------------------------------------"
 echo "Crio a imagem final"
 docker-compose -f "docker-compose.yml" -f "docker-compose.cd-final.yml" build
-docker-compose -f "docker-compose.yml" -f "docker-compose.cd-final.yml" push
+#docker-compose -f "docker-compose.yml" -f "docker-compose.cd-final.yml" push
 echo "-------------------------------------------"
 
 
